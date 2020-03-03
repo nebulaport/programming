623355cd727b82e37e4575efb775501e7328a37b
zls <cloudorwiwiwen@gmail.com>
2020-03-03 15:26:25 +0800

    优化

--- src/luogu/SingleplankBridge.java
+++ src/luogu/SingleplankBridge.java
@@ -24,21 +24,17 @@ import java.util.Scanner;
  */
 public class SingleplankBridge {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int L =in.nextInt();
+        Scanner in = new Scanner(System.in);
+        int l=in.nextInt();
         in.nextLine();
-        int N =in.nextInt();
+        int n=in.nextInt();
         in.nextLine();
-        int[]a=new int[N];
-        for (int i = 0; i <N ; i++) {
-            a[i]=in.nextInt();
+        int max=0,min=0;
+        for (int i = 0; i <n ; i++) {
+            int p=in.nextInt();
+            max=Math.max(max,Math.max(p,l-p+1));
+            min=Math.max(min,Math.min(p,l-p+1));
         }
-       int min=0,max=0;
-        for (int i = 0; i < N; i++) {
-            min=Math.max(min,Math.min(a[i],L+1-a[i]));
-            max=Math.max(max,Math.max(a[i],L+1-a[i]));
-        }
-
         System.out.println(min+" "+max);
     }
 }
